bce997d14b701f31925e02443f643f394709c1bc
Jeff Albrecht <geospatialjeff@gmail.com>
2019-06-07 16:08:17 -0700

    ading default resource names

---
+++ stac_updater/names.py
@@ -0,0 +1,11 @@
+
+sns_topic = 'NewSTACItemTopic'
+static_queue = 'StaticSTACQueue'
+static_dlq = 'StaticDLQ'
+static_sns_sub = 'StaticSNSSubscription'
+static_sqs_policy = 'StaticSQSPolicy'
+
+dynamic_queue = 'DynamicSTACQueue'
+dynamic_dlq = 'DynamicDLQ'
+dynamic_sns_sub = 'DynamicSNSSubscription'
+dynamic_sqs_policy = 'DynamicSQSPolicy'
