0ac8ecf6208b2567c55d6f1bc9799210d4d71c23
Jeff Albrecht <geospatialjeff@gmail.com>
2019-06-07 16:09:57 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('./requirements.txt') as reqs:
+    requirements = [line.rstrip() for line in reqs]
+
+setup(name="STAC Updater",
+      version='0.1',
+      author='Jeff Albrecht',
+      author_email='geospatialjeff@gmail.com',
+      packages=find_packages(exclude=['package']),
+      install_requires = requirements,
+      entry_points= {
+          "console_scripts": [
+              "stac-updater=stac_updater.cli:stac_updater"
+          ]},
+      include_package_data=True
+      )
