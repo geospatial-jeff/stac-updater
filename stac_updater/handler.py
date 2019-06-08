3a205a3b4539626e64b079e63262eb4f2bc8dbd6
Jeff Albrecht <geospatialjeff@gmail.com>
2019-06-07 17:23:46 -0700

    adding lambda function to update static catalog via satstac

--- stac_updater/handler.py
+++ stac_updater/handler.py
@@ -1,6 +1,8 @@
 import json
 import os
+from pathlib import Path
 
+from satstac import Collection, Item
 import boto3
 
 from stac_updater import names
@@ -10,6 +12,8 @@ sns_client = boto3.client('sns')
 ACCOUNT_ID = boto3.client('sts').get_caller_identity()['Account']
 REGION = os.getenv('REGION')
 CAT_ROOT = os.getenv('CAT_ROOT_URL')
+ITEM_PATH = os.getenv('ITEM_PATH')
+ITEM_NAME = os.getenv('ITEM_NAME')
 
 
 def kickoff(event, context):
@@ -20,3 +24,31 @@ def kickoff(event, context):
         TopicArn=f"arn:aws:sns:{REGION}:{ACCOUNT_ID}:{names.sns_topic}",
         message=stac_item
     )
+
+def static_stac_updater(event, context):
+
+    for record in event['Records']:
+        stac_item = record['body']
+
+        # Determine collection path from stac item links
+        links = {}
+        for link in stac_item['links']:
+            if link['rel'] == 'collection':
+                links.update({'collection': link['href']})
+            elif link['rel'] == 'self':
+                links.update({'self': link['href']})
+        item_path = Path(links['self']).parent
+        collection_path = (item_path / links['collection']).resolve()
+        collection_path = 'https://' + str(collection_path).split('https:/')[-1]
+
+        # Open collection and save STAC Item
+        col = Collection.open(collection_path)
+        item = Item(stac_item)
+
+        kwargs = {'item': item}
+        if ITEM_PATH:
+            kwargs.update({'path': ITEM_PATH})
+        if ITEM_NAME:
+            kwargs.update({'filename': ITEM_NAME})
+        col.add_item(**kwargs)
+        col.save()
