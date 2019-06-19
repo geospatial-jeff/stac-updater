b4e58024cd85efe47378deae8f80269489a04c7b
Jeff Albrecht <geospatialjeff@gmail.com>
2019-06-18 20:44:30 -0700

    adding support for s3 event source to kickoff lambda function

--- stac_updater/handler.py
+++ stac_updater/handler.py
@@ -5,17 +5,30 @@ from satstac import Collection, Item
 import boto3
 
 sns_client = boto3.client('sns')
+s3_res = boto3.resource('s3')
 
 ACCOUNT_ID = boto3.client('sts').get_caller_identity()['Account']
 REGION = os.getenv('REGION')
 
 
 def kickoff(event, context):
+    event_source = os.getenv('EVENT_SOURCE')
+
+    # Load payload based on event source
+    if event_source == "s3":
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = event['Records'][0]['s3']['object']['key']
+        content_object = s3_res.Object(bucket, key)
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        payload = json.loads(file_content)
+    else:
+        # Default is lambda
+        payload = event
 
     try:
-        coll_name = event['stac_item']['properties']['collection']
+        coll_name = payload['stac_item']['properties']['collection']
     except KeyError:
-        coll_name = event['stac_item']['collection']
+        coll_name = payload['stac_item']['collection']
 
     sns_client.publish(
         TopicArn=f"arn:aws:sns:{REGION}:{ACCOUNT_ID}:newStacItemTopic",
